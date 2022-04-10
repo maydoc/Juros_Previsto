a01a36f2d27c553e3ed2846f2061644022d4c2c0
Mayron <mayron_doc@outlook.com>
2022-04-10 20:48:13 -0300

    Versão 1.0 - Consultar Releases para mais detalhes

--- Juros_Previsto/MainWindow.xaml.cs
+++ Juros_Previsto/MainWindow.xaml.cs
@@ -2,97 +2,190 @@
 using System;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Juros_Previsto {
     /// <summary>
     /// Interação lógica para MainWindow.xam
     /// </summary>
     public partial class MainWindow : Window {
-
         
         public MainWindow() {
             DataContext = this;
-            
         }
-        private void Switch_Mode_Theme(bool initialize = false) {
+        private void Switch_Mode_Theme(object sender, RoutedEventArgs e) {
             PaletteHelper paletteHelper = new PaletteHelper();
             ITheme theme = paletteHelper.GetTheme();
 
-            if (theme.GetBaseTheme() == BaseTheme.Dark && initialize) {
-                Button_Close.Foreground = Brushes.White;
-                Button_DarkLight_Mode.Foreground = Brushes.White;
-                Button_DarkLight_Mode.Background = (Brush) new BrushConverter().ConvertFromString("#424242");
-
-            }
-            else if (initialize) {
-                theme.SetBaseTheme(Theme.Dark);
+            if (theme.GetBaseTheme() == BaseTheme.Dark) {
+                theme.SetBaseTheme(Theme.Light);
                 paletteHelper.SetTheme(theme);
-                Button_Close.Foreground = Brushes.Black;
-                Button_DarkLight_Mode.Foreground = Brushes.Black;
-                Button_DarkLight_Mode.Background = Brushes.White;
             }
             else {
-                if (theme.GetBaseTheme() == BaseTheme.Dark) {
-                    theme.SetBaseTheme(Theme.Light);
-                    paletteHelper.SetTheme(theme);
-                    Button_Close.Foreground = Brushes.Black;
-                    Button_DarkLight_Mode.Foreground = Brushes.Black;
-                    Button_DarkLight_Mode.Background = Brushes.White;
-                }
-                else {
-                    theme.SetBaseTheme(Theme.Dark);
-                    paletteHelper.SetTheme(theme);
-                    Button_Close.Foreground = Brushes.White;
-                    Button_DarkLight_Mode.Foreground = Brushes.White;
-                    Button_DarkLight_Mode.Background = (Brush) new BrushConverter().ConvertFromString("#424242");
-                }
+                theme.SetBaseTheme(Theme.Dark);
+                paletteHelper.SetTheme(theme);
             }
         }
-        private void Button_DarkLight_Mode_Checked(object sender, RoutedEventArgs e) {
-            Switch_Mode_Theme();
-        }
-
-        private void Button_DarkLight_Mode_Unchecked(object sender, RoutedEventArgs e) {
-            Switch_Mode_Theme();
-        }
-
         private void Button_Close_Click(object sender, RoutedEventArgs e) {
             Application.Current.Shutdown();
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e) {
-            Switch_Mode_Theme(true);
+        private async void Button_Refresh_Click(object sender, RoutedEventArgs e) {
+            Animate_Text(Value_Nominal, double.Parse(Value_Nominal.Text.Substring(2)), 0, milliseconds:200);
+            Animate_Text(Value_Total, double.Parse(Value_Total.Text.Substring(2)), 0, milliseconds:200);
+            Animate_Text(Value_Juros, double.Parse(Value_Juros.Text.Substring(2)), 0, milliseconds:200);
+            Animate_Text(Total_Days, double.Parse(Total_Days.Text.Replace(" dias", "")), 0, false, milliseconds:200);
+            await Task.Delay(201);
+            Button_Calculate_Click(null, null);
+        }
+        private async void Animate_Text(TextBlock textBlock, double to, double from, bool doubleformat = true, int milliseconds = 500) { 
+            DoubleAnimation animaiton = new DoubleAnimation(to, from, TimeSpan.FromMilliseconds(milliseconds));
+            Rectangle rectangle = new Rectangle() { Width = to };
+            rectangle.SizeChanged += (sender, args) => {
+                textBlock.Text = doubleformat ? rectangle.Width.ToString("C2") : rectangle.Width.ToString("N0") + " dias";
+            };
+            MainGrid.Children.Add(rectangle);
+            
+            rectangle.BeginAnimation(WidthProperty, animaiton);
+            await Task.Delay(500);
+            MainGrid.Children.Remove(rectangle);
         }
 
-        private void Button_Refresh_Click(object sender, RoutedEventArgs e) {
-
+        private void Switch_View_Date_Change(object sender, RoutedEventArgs e) {
+            switch (Transitioner_Days.SelectedIndex)
+            {
+                case 0:
+                    Transitioner_Days.SelectedIndex = 1;
+                    break;
+                case 1:
+                    Transitioner_Days.SelectedIndex = 0;
+                    break;
+                default:
+                    break;
+            }
         }
-
-        private void Switch_View_Date_Checked(object sender, RoutedEventArgs e) {
-            Transitioner_Days.SelectedIndex = 1;
-        }
-
-        private void Switch_View_Date_Unchecked(object sender, RoutedEventArgs e) {
-            Transitioner_Days.SelectedIndex = 0;
-        }
-
-        private void Check_Calc_Auto_Checked(object sender, RoutedEventArgs e) {
-            Button_Calculate.Visibility = Visibility.Hidden;
-        }
-
-        private void Check_Calc_Auto_Unchecked(object sender, RoutedEventArgs e) {
-            Button_Calculate.Visibility = Visibility.Visible;
-        }
-
         private void Button_Calculate_Click(object sender, RoutedEventArgs e) {
+
+            if (!Scan_Values()) { return; }
+
             BaseCalculoJuros juros = new BaseCalculoJuros();
-            juros.OpenCalc(DateTime.Parse(DatePicker_Inicial.Text), DateTime.Parse(DatePicker_Final.Text), double.Parse(TextBox_Valor.Text), double.Parse(TextBox_Juros.Text), (bool) Switch_Type_Juros.IsChecked, (bool) Swtich_Period_Juros.IsChecked, int.Parse(TextBox_Carencia.Text));
+            int carencia = 0;
+            if(TextBox_Carencia.Text != String.Empty) {
+                carencia = int.Parse(TextBox_Carencia.Text);
+            }
+            juros.OpenCalc(DateTime.Parse(DatePicker_Inicial.Text),
+                DateTime.Parse(DatePicker_Final.Text),
+                double.Parse(TextBox_Valor.Text),
+                double.Parse(TextBox_Juros.Text),
+                (bool) Switch_Type_Juros.IsChecked,
+                (bool) Swtich_Period_Juros.IsChecked,
+                carencia);
+
             juros.ExecuteCalc();
-            Value_Nominal.Text = juros.Valor_Nominal.ToString("C2");
-            Value_Total.Text = juros.Valor_Total.ToString("C2");
-            Value_Juros.Text = juros.Valor_Juros.ToString("C2");
-            Total_Days.Text = juros.Dias_Totais.ToString() + " dias";
+
+            Animate_Text(Value_Nominal, double.Parse(Value_Nominal.Text.Substring(2)), juros.Valor_Nominal);
+            Animate_Text(Value_Total, double.Parse(Value_Total.Text.Substring(2)), juros.Valor_Total);
+            Animate_Text(Value_Juros, double.Parse(Value_Juros.Text.Substring(2)), juros.Valor_Juros);
+            Animate_Text(Total_Days, double.Parse(Total_Days.Text.Replace(" dias", "")), juros.Dias_Totais, false);
+        }
+        private bool Scan_Values() {
+            try {
+                DateTime.Parse(DatePicker_Inicial.Text);
+            }
+            catch(Exception) {
+                TextDialog.Text = "Digite uma data inicial válida!";
+                DialogHostMain.IsOpen = true;
+                return false;
+            }
+            try {
+                DateTime.Parse(DatePicker_Final.Text);
+            }
+            catch(Exception) {
+                TextDialog.Text = "Digite uma data final válida!";
+                DialogHostMain.IsOpen = true;
+                return false;
+            }
+            if(TextBox_Juros.Text == String.Empty) {
+                TextDialog.Text = "Informe o juros!";
+                DialogHostMain.IsOpen = true;
+                return false;
+            }
+            if(TextBox_Valor.Text == String.Empty) {
+                TextDialog.Text = "Digite o valor!";
+                DialogHostMain.IsOpen = true;
+                return false;
+            }
+            return true;
+        }
+        private void Calc_Auto() {
+            if (!CalcAutoEnable) { return; }
+            try {
+                BaseCalculoJuros juros = new BaseCalculoJuros();
+                int carencia = 0;
+                if(TextBox_Carencia.Text != String.Empty) {
+                    carencia = int.Parse(TextBox_Carencia.Text);
+                }
+                juros.OpenCalc(DateTime.Parse(DatePicker_Inicial.Text),
+                    DateTime.Parse(DatePicker_Final.Text),
+                    double.Parse(TextBox_Valor.Text),
+                    double.Parse(TextBox_Juros.Text),
+                    (bool) Switch_Type_Juros.IsChecked,
+                    (bool) Swtich_Period_Juros.IsChecked,
+                    carencia);
+
+                juros.ExecuteCalc();
+
+                Animate_Text(Value_Nominal, double.Parse(Value_Nominal.Text.Substring(2)), juros.Valor_Nominal);
+                Animate_Text(Value_Total, double.Parse(Value_Total.Text.Substring(2)), juros.Valor_Total);
+                Animate_Text(Value_Juros, double.Parse(Value_Juros.Text.Substring(2)), juros.Valor_Juros);
+                Animate_Text(Total_Days, double.Parse(Total_Days.Text.Replace(" dias", "")), juros.Dias_Totais, false);
+                
+                if (AvisoCalcAuto.Height == 46) {
+                    DoubleAnimation switchAnimation = new DoubleAnimation(46, 30, TimeSpan.FromMilliseconds(350));
+                    AvisoCalcAuto.BeginAnimation(HeightProperty, switchAnimation);
+                }
+            }
+            catch(Exception) {
+
+                if(AvisoCalcAuto.Height == 30) {
+                    DoubleAnimation switchAnimation = new DoubleAnimation(30, 46, TimeSpan.FromMilliseconds(350));
+                    AvisoCalcAuto.BeginAnimation(HeightProperty, switchAnimation);
+                }
+            }
+        }
+        bool CalcAutoEnable = false;
+        private void Check_Calc_Auto_Click(object sender, RoutedEventArgs e) {
+            switch(Check_Calc_Auto.IsChecked) {
+                case true:
+                    CalcAutoEnable = true;
+                    Calc_Auto();
+                    break;
+                case false:
+                    CalcAutoEnable = false;  
+                    if(AvisoCalcAuto.Height == 46) {
+                        DoubleAnimation switchAnimation = new DoubleAnimation(46, 30, TimeSpan.FromMilliseconds(350));
+                        AvisoCalcAuto.BeginAnimation(HeightProperty, switchAnimation);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void DatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e) {
+            Calc_Auto();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e) {
+            Calc_Auto();
+        }
+
+        private void Switch_Type_Click(object sender, RoutedEventArgs e) {
+            Calc_Auto();
         }
     }
 }
